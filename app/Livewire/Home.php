8dc8746003376927967d5e80f56bd5d7880185f9
Lucas Cipriano <lucasncborges@gmail.com>
2025-09-26 23:25:29 -0300

    test

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -4,14 +4,24 @@ declare(strict_types=1);
 
 namespace App\Livewire;
 
-use Illuminate\Contracts\View\View;
+use App\Models\Post;
+use App\Models\Subreddit;
+use App\Models\User;
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 final class Home extends Component
 {
     public function render(): View|Factory
     {
-        return view('livewire.home');
+        $user = User::query()->where('email', 'lucas@gmail.com')->first();
+        $posts = $user->posts;
+        $userCount = User::query()->count();
+        $postsCount = Post::query()->count();
+        $subRedditsCount = Subreddit::query()->count();
+
+        return view('livewire.home',
+            ['postsCount' => $postsCount, 'subRedditsCount' => $subRedditsCount, 'userCount' => $userCount, 'posts' => $posts]);
     }
 }
